ce8e3a73a9ffae44d7774af3f3550b6b75d0b535
hanseungjae <gkstmdwo100@naver.com>
2025-08-02 14:00:48 +0900

    feat: 유저 이름 유니크 제약조건 추가 1. Entity - 유니크 조건 추가 2. Service - try-catch 문으로 23505 유니크 에러 잡기

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+    ConflictException,
+    Injectable,
+    InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -14,7 +18,15 @@ export class AuthService {
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<User> {
         const { username, password } = authCredentialsDto;
         const user = this.userRepository.create({ username, password });
-        await this.userRepository.save(user);
+        try {
+            await this.userRepository.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Exsiting Username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
         return user;
     }
 }
